ca6175106f56e796225e8b0c3ba539930cb2b797
solardude2 <93727767+solardude2@users.noreply.github.com>
2024-03-28 17:48:55 -0400

    Actuator delay .105

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -144,7 +144,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
     ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.00]]
     ret.lateralTuning.pid.kf = 0.00004   # full torque for 20 deg at 80mph means 0.00007818594
-    ret.steerActuatorDelay = 0.1  # Default delay, not measured yet
+    ret.steerActuatorDelay = 0.105  # Default delay, not measured yet
     ret.tireStiffnessFactor = 0.444  # not optimized yet
 
     ret.steerLimitTimer = 0.4
