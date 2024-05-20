d4dae42a67eb91aa7d2b78486fbf895e07a5bc21
solardude2 <93727767+solardude2@users.noreply.github.com>
2024-05-20 11:28:10 -0400

    Steer delay

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -144,7 +144,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
     ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.00]]
     ret.lateralTuning.pid.kf = 0.00004   # full torque for 20 deg at 80mph means 0.00007818594
-    ret.steerActuatorDelay = 0.1  # Default delay, not measured yet
+    ret.steerActuatorDelay = 0.105  # Default delay, not measured yet
 
     ret.steerLimitTimer = 0.4
     ret.radarTimeStep = 0.0667  # GM radar runs at 15Hz instead of standard 20Hz
